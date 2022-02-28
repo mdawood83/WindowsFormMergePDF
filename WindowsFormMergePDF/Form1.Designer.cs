b2b950bb9b6806ae2e241e9b0a0bfc22b7b9858f
Mina Dawood <mina.latif@live.com>
2022-02-28 10:38:28 -0500

    Add project files.

---
+++ WindowsFormMergePDF/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace WindowsFormMergePDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.merge_btn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.remove_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.split_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // merge_btn
+            // 
+            this.merge_btn.BackColor = System.Drawing.Color.ForestGreen;
+            this.merge_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.merge_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.merge_btn.ForeColor = System.Drawing.Color.White;
+            this.merge_btn.Location = new System.Drawing.Point(51, 58);
+            this.merge_btn.Name = "merge_btn";
+            this.merge_btn.Size = new System.Drawing.Size(93, 37);
+            this.merge_btn.TabIndex = 0;
+            this.merge_btn.Text = "&Merge";
+            this.merge_btn.UseVisualStyleBackColor = false;
+            this.merge_btn.Click += new System.EventHandler(this.merge_btn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(20, 130);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(513, 186);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            // 
+            // remove_btn
+            // 
+            this.remove_btn.BackColor = System.Drawing.Color.Red;
+            this.remove_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.remove_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.remove_btn.ForeColor = System.Drawing.Color.White;
+            this.remove_btn.Location = new System.Drawing.Point(284, 58);
+            this.remove_btn.Name = "remove_btn";
+            this.remove_btn.Size = new System.Drawing.Size(92, 37);
+            this.remove_btn.TabIndex = 2;
+            this.remove_btn.Text = "&Remove";
+            this.remove_btn.UseVisualStyleBackColor = false;
+            this.remove_btn.Click += new System.EventHandler(this.remove_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.LightSteelBlue;
+            this.label1.Location = new System.Drawing.Point(192, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "DRAG && DROP PDF FILES HERE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDoubleClick);
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.BackColor = System.Drawing.Color.Crimson;
+            this.clear_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear_btn.ForeColor = System.Drawing.Color.White;
+            this.clear_btn.Location = new System.Drawing.Point(402, 58);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(92, 37);
+            this.clear_btn.TabIndex = 4;
+            this.clear_btn.Text = "&Clear All";
+            this.clear_btn.UseVisualStyleBackColor = false;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label2.Location = new System.Drawing.Point(394, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Powered By: Mina Dawood";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.ForeColor = System.Drawing.Color.LightSteelBlue;
+            this.label3.Location = new System.Drawing.Point(207, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "OR DOUBLE CLICK HERE";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.label3_MouseDoubleClick);
+            // 
+            // split_btn
+            // 
+            this.split_btn.BackColor = System.Drawing.Color.SlateBlue;
+            this.split_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.split_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.split_btn.ForeColor = System.Drawing.Color.White;
+            this.split_btn.Location = new System.Drawing.Point(166, 58);
+            this.split_btn.Name = "split_btn";
+            this.split_btn.Size = new System.Drawing.Size(93, 37);
+            this.split_btn.TabIndex = 7;
+            this.split_btn.Text = "&Split";
+            this.split_btn.UseVisualStyleBackColor = false;
+            this.split_btn.Click += new System.EventHandler(this.split_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(552, 338);
+            this.Controls.Add(this.split_btn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.remove_btn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.merge_btn);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Merge PDF";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button merge_btn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button remove_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clear_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button split_btn;
+    }
+}
+
---
+++ WindowsFormMergePDF/Form1.cs
@@ -0,0 +1,296 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormMergePDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void DoMerge()
+        {
+            List<string> ret = new List<string>();
+
+            foreach (string item in listBox1.Items)
+            {
+                ret.Add(item);
+            }
+
+            string[] files = ret.ToArray();
+
+            const string filename = "Merged_PDF.pdf";
+
+            if (File.Exists(filename))
+                File.Delete(filename);
+
+            MergeMultiplePDFIntoSinglePDF(filename, files);
+        }
+
+        private void DoSplit(string[] files)
+        {
+            // Get a fresh copy of the sample PDF file
+            //string filename = "Portable Document Format.pdf";
+            //File.Copy(Path.Combine("../../../../PDFs/", filename),
+            //  Path.Combine(Directory.GetCurrentDirectory(), filename), true);
+
+            string downloadPath = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + @"Downloads\Splitted_PDF\";
+
+            if (!Directory.Exists(downloadPath))
+            {
+                Directory.CreateDirectory(downloadPath);
+            }
+
+            foreach (string file in files)
+            {
+                // Open the file
+                PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+                string name = Path.GetFileNameWithoutExtension(file);
+
+                for (int i = 0; i < inputDocument.PageCount; i++)
+                {
+                    // Create new document
+                    PdfDocument outputDocument = new PdfDocument();
+                    outputDocument.Version = inputDocument.Version;
+                    outputDocument.Info.Title =
+                      String.Format("Page {0} of {1}", i + 1, inputDocument.Info.Title);
+                    outputDocument.Info.Creator = inputDocument.Info.Creator;
+
+                    // Add the page and save it
+                    outputDocument.AddPage(inputDocument.Pages[i]);
+
+                    string outputPath = String.Format("{0} - Page {1}.pdf", name, i + 1);
+
+                    outputDocument.Save(downloadPath + outputPath);
+                }
+
+                Process.Start(downloadPath);
+            }
+        }
+
+        //Get Filename
+        //private string GetFileName(string path)
+        //{
+        //    return Path.GetFileName(path);
+        //}
+
+
+        /// <summary>
+        /// Drag/Drop files
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            //Take Dropped files and store in Array
+            string[] droppedFiles = (string[]) e.Data.GetData(DataFormats.FileDrop);
+
+            //Loop through all Dropped files and Display them
+            foreach (string file in droppedFiles)
+            {
+                string AllowedExtension = Path.GetExtension(file);
+                
+                if (AllowedExtension.ToUpper() == ".PDF")
+                {
+                    //string fileName = Path.GetFileName(file);
+                    //MessageBox.Show("You dropped in [\"" + fileName + "\"]");
+                    listBox1.Items.Add(file);
+                }
+                else
+                {
+                    MessageBox.Show("Allowed Extension is PDF Files only...");
+                }
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private static void MergeMultiplePDFIntoSinglePDF(string outputFilePath, string[] pdfFiles)
+        {
+            outputFilePath = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + @"Downloads\Merged_PDF.pdf";
+
+            //Console.WriteLine("Merging started.....");
+            PdfDocument outputPDFDocument = new PdfDocument();
+            foreach (string pdfFile in pdfFiles)
+            {
+                if (pdfFile.Contains("Merged_PDF"))
+                {
+                    continue;
+                }
+
+                PdfDocument inputPDFDocument = PdfReader.Open(pdfFile, PdfDocumentOpenMode.Import);
+
+                outputPDFDocument.Version = inputPDFDocument.Version;
+                foreach (PdfPage page in inputPDFDocument.Pages)
+                {
+                    outputPDFDocument.AddPage(page);
+                }
+            }
+            if (outputPDFDocument.PageCount == 0) throw new Exception($"Page Count is {outputPDFDocument.PageCount}");
+
+            outputPDFDocument.Save(outputFilePath);
+            
+            Process.Start(outputFilePath);
+        }
+
+        private void merge_btn_Click(object sender, EventArgs e)
+        {
+            while (listBox1.Items.Count > 0)
+            {
+                DoMerge();
+                return;
+            }
+
+            MessageBox.Show("The Item List is empty...");
+        }
+
+        private void remove_btn_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+
+            if (index == -1)
+            {
+                MessageBox.Show("Please select the item you wish to remove...");
+                return;
+            }
+
+            listBox1.Items.RemoveAt(index);
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Multiselect = true;
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {   
+                    string[] arrAllFiles = dialog.FileNames;
+
+                    foreach (var item in arrAllFiles)
+                    {
+                        if (Path.GetExtension(item).ToUpper() == ".PDF")
+                        {
+                            listBox1.Items.Add(item);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Allowed Extension is a PDF Files only...");
+                        }
+                    }
+
+                    //string file = dialog.FileName;
+                    //string AllowedExtension = Path.GetExtension(file);
+
+                    //if (AllowedExtension.ToUpper() == ".PDF")
+                    //{
+                    //    //string fileName = Path.GetFileName(file);
+                    //    //MessageBox.Show("You dropped in [\"" + fileName + "\"]");
+                    //    listBox1.Items.Add(file);
+                    //}
+                    //else
+                    //{
+                    //    MessageBox.Show("Allowed Extension is a PDF Files only...");
+                    //}
+                }
+            }
+        }
+
+        private void label1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Multiselect = true;
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string[] arrAllFiles = dialog.FileNames;
+
+                    foreach (var item in arrAllFiles)
+                    {
+                        if (Path.GetExtension(item).ToUpper() == ".PDF")
+                        {
+                            listBox1.Items.Add(item);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Allowed Extension is a PDF Files only...");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void label3_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Multiselect = true;
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string[] arrAllFiles = dialog.FileNames;
+
+                    foreach (var item in arrAllFiles)
+                    {
+                        if (Path.GetExtension(item).ToUpper() == ".PDF")
+                        {
+                            listBox1.Items.Add(item);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Allowed Extension is a PDF Files only...");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void split_btn_Click(object sender, EventArgs e)
+        {
+            while (listBox1.Items.Count > 0)
+            {
+                string[] files = listBox1.Items.Cast<string>().ToArray();
+
+                DoSplit(files);
+                return;
+            }
+
+            MessageBox.Show("The Item List is empty...");
+        }
+
+        //FileInfo f = new FileInfo(listBox1.SelectedItem.ToString());
+        //var ss = f.OpenRead();
+
+        //                if (ss.Length > 0)
+        //                {
+        //                    axAcroPDF1.src = f.FullName;
+        //                }
+}
+}
